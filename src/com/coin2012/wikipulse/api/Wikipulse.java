f8c9eae6513a3b63cd1506ca342dd0266d1d2a04
Harrison Mfula <hmfula@yahoo.com>
2012-11-11 00:02:19 +0200

    Refactor: removed unused imports

--- src/com/coin2012/wikipulse/api/Wikipulse.java
+++ src/com/coin2012/wikipulse/api/Wikipulse.java
@@ -2,14 +2,13 @@ package com.coin2012.wikipulse.api;
 
 import static spark.Spark.get;
 
-import com.coin2012.wikipulse.identification.Identifiable;
-import com.coin2012.wikipulse.identification.Identifier;
-
 import spark.Request;
 import spark.Response;
 import spark.Route;
+
 /**
  * Main class. Defines the entry point into the system.
+ * 
  * @author harrison mfula
  * @author Karsten packmohr
  * @since 10-11-2012
@@ -23,7 +22,7 @@ public class Wikipulse {
 			public Object handle(Request request, Response response) {
 				String category = "Category:United_States_presidential_election,_2012";
 				WikiPulseService wikipulseService = new WikiPulseServiceImpl();
-				return  wikipulseService.getNewsForCategory(category);
+				return wikipulseService.getNewsForCategory(category);
 			}
 		});
 
