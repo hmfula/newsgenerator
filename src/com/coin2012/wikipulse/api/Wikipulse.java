9822f341604301f3f203cc115cbc5919bbefd8ab
karstenpackmohr <karsten.packmohr@googlemail.com>
2012-11-20 21:09:14 +0100

    Wikipulse now works on a tomcat

--- src/com/coin2012/wikipulse/api/Wikipulse.java
+++ src/com/coin2012/wikipulse/api/Wikipulse.java
@@ -5,6 +5,7 @@ import static spark.Spark.get;
 import spark.Request;
 import spark.Response;
 import spark.Route;
+import spark.servlet.SparkApplication;
 
 /**
  * Main class. Defines the entry point into the system.
@@ -13,31 +14,38 @@ import spark.Route;
  * @author karsten packmohr
  * @since 10-11-2012
  */
-public class Wikipulse {
+public class Wikipulse implements SparkApplication {
 
-	public static void main(String[] args) {
+	 public static void main(String[] args) {
+		createRESTRoutes();
+	 }
 
+	public void init() {
+		createRESTRoutes();
+	}
+
+	private static void createRESTRoutes() {
 		get(new Route("/News") {
 			@Override
 			public Object handle(Request request, Response response) {
-				
+
 				String category = "Category:United_States_presidential_election,_2012";
 				WikipulseService wikipulseService = new WikipulseServiceImpl();
+				response.type("application/json; charset=utf-8");
 				return wikipulseService.getNewsForCategory(category);
 			}
 		});
-		
+
 		get(new Route("/MostReadArticlesInCategory") {
 			@Override
 			public Object handle(Request request, Response response) {
 				String category = "Category:" + request.queryParams("category");
 				WikipulseService wikipulseService = new WikipulseServiceImpl();
 				response.type("application/json; charset=utf-8");
-				String return_set = "callback(" +wikipulseService.getMostReadTitlesForCategory(category) + ")";
+				String return_set = wikipulseService.getMostReadTitlesForCategory(category);
 				return return_set;
 			}
 		});
-
 	}
 
 }
