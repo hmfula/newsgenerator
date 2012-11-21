d719f964385619e8df33f8813f979fece9dc4d84
Harrison Mfula <hmfula@yahoo.com>
2012-11-22 00:36:27 +0200

    Fix: merge

--- src/com/coin2012/wikipulse/api/Wikipulse.java
+++ src/com/coin2012/wikipulse/api/Wikipulse.java
@@ -2,10 +2,12 @@ package com.coin2012.wikipulse.api;
 
 import static spark.Spark.get;
 
+
 import spark.Request;
 import spark.Response;
 import spark.Route;
 import spark.servlet.SparkApplication;
+
 
 /**
  * Main class. Defines the entry point into the system.
@@ -46,6 +48,31 @@ public class Wikipulse implements SparkApplication {
 				return return_set;
 			}
 		});
-	}
+		//Example use http://localhost:4567/ExernalLinkSearch?url=www.mitty.romney.com
+		//Example can be used for result verification. http://en.wikipedia.org/w/api.php?action=query&list=exturlusage&euquery=www.mittromney.com&eulimit=30&eunamespace=0&format=json
+		get(new Route("/ExernalLinkSearch") {
+				@Override
+				public Object handle(Request request, Response response) {
+					String url = request.queryParams("url");
+					WikipulseService wikipulseService = new WikipulseServiceImpl();
+					response.type("application/json; charset=utf-8");
+					String externallyLinkePages = "callback(" + wikipulseService.searchForPagesReferencing(url) +")";
+					return externallyLinkePages;
+				}
+			});
+		 
+	
+	       //Example use  http://localhost:4567/FreeTextSearch?&srsearch=2012 US Elections
+			get(new Route("/FreeTextSearch") {
+					@Override
+					public Object handle(Request request, Response response) {
+						String searchText = request.queryParams("srsearch");
+						WikipulseService wikipulseService = new WikipulseServiceImpl();
+						response.type("application/json; charset=utf-8");
+						String  snippetPages ="callback(" + wikipulseService.searchForPagesThatMatch(searchText) + ")";
+						return snippetPages;
+					}
 
+				});
+	}
 }
