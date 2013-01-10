8c6953148e221c67f3b0f0886813e7da2110a239
Harrison Mfula <hmfula@yahoo.com>
2013-01-11 01:18:17 +0200

    Improvement: Fixed possible memory leak (database opening & closing procedure)

--- src/com/coin2012/wikipulse/extraction/hsqldb/HsqldbManager.java
+++ src/com/coin2012/wikipulse/extraction/hsqldb/HsqldbManager.java
@@ -5,6 +5,7 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -16,14 +17,17 @@ import com.coin2012.wikipulse.extraction.utils.TimestampGenerator;
 import com.coin2012.wikipulse.extraction.utils.models.Change;
 
 public class HsqldbManager {
-	static Logger logger = Logger.getLogger("HsqldbManager.class");
+	static Logger logger = Logger.getLogger(ClassLoader.class.getSimpleName());
+	private  static Connection connection;
+	
 	
 	public static String getTimestampForLastSavedChange() {
 		String timestamp = null;
+		PreparedStatement prepStatement = null;
 		try {
-			Connection connection = DriverManager.getConnection("jdbc:hsqldb:mem:wikipulsememdb", "SA", "");
-			PreparedStatement psq = connection.prepareStatement("SELECT * FROM changes");
-			ResultSet rs = psq.executeQuery();
+			connection = getConnection();
+			prepStatement = connection.prepareStatement("SELECT * FROM changes");
+			ResultSet rs = prepStatement.executeQuery();
 			while (rs.next()) {
 				if (rs.isFirst()) {
 					timestamp = rs.getString(1);
@@ -32,14 +36,17 @@ public class HsqldbManager {
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
+		}finally{
+			cleanup(connection, prepStatement);	
 		}
 		return timestamp;
 	}
-
+	
 	public static void saveChangesToMemDB(List<Change> changes) {
+		PreparedStatement prepStatement = null;
 		try {
-			Connection connection = DriverManager.getConnection("jdbc:hsqldb:mem:wikipulsememdb", "SA", "");
-			PreparedStatement prepStatement = connection.prepareStatement("INSERT INTO changes VALUES (?,?)");
+		 connection = getConnection();
+			 prepStatement = connection.prepareStatement("INSERT INTO changes VALUES (?,?)");
 			for (Change change : changes) {
 				try {
 					prepStatement.clearParameters();
@@ -51,9 +58,32 @@ public class HsqldbManager {
 				}
 			}
 		} catch (SQLException e) {
-			// TODO Auto-generated catch block
+			logger.info("Failed to create  a Preparedstatement because of  " + e.getCause());
+			e.printStackTrace();
+		}finally{
+			cleanup(connection, prepStatement);	
+		}
+	}
+
+	private static void cleanup(Connection connection, Statement prepStatement) {
+		try {
+		if(prepStatement != null){
+				prepStatement.close();
+			} 
+		if(connection != null){
+			connection.close();
+		} 
+		}catch (SQLException e) {
+			logger.info("Failed to close the connection to the database because of : " + e.getCause() );
 			e.printStackTrace();
 		}
+	}
+
+	private static Connection getConnection() throws SQLException {
+		if(connection != null){
+			return connection;
+		} 
+		return DriverManager.getConnection("jdbc:hsqldb:mem:wikipulsememdb", "SA", "");
 	}
 
 	public static void clearOldChangesFromMemDB(String timestamp) {
@@ -69,19 +99,29 @@ public class HsqldbManager {
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
+		}finally{
+			try {
+				cleanup(connection, rs.getStatement());
+			} catch (SQLException e) {
+				logger.info("Failed to close the connection to the database because of : " + e.getCause() );
+				e.printStackTrace();
+			}	
 		}
 		
 	}
 
 	public static ResultSet getAllChangesFromMemDB() {
 		ResultSet rs = null;
+		PreparedStatement prepStatement = null;
 		try {
-			Connection connection = DriverManager.getConnection("jdbc:hsqldb:mem:wikipulsememdb", "SA", "");
-			PreparedStatement psq = connection.prepareStatement("SELECT * FROM changes for update");
-			rs = psq.executeQuery();
+			connection = getConnection();
+			prepStatement = connection.prepareStatement("SELECT * FROM changes for update");
+			rs = prepStatement.executeQuery();
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
+		}finally{
+			cleanup(connection, prepStatement);
 		}
 		return rs;
 	}
@@ -102,52 +142,58 @@ public class HsqldbManager {
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
+		}finally{
+			try {
+				cleanup(connection, rs.getStatement());
+			} catch (SQLException e) {
+				logger.info("Failed to close the connection to the database because of : " + e.getCause() );
+				e.printStackTrace();
+			}	
 		}
 		return map;
 	}
 	
 	public static void saveUserInteractionInDB(String News){
+		PreparedStatement prepStatement = null;
+		ResultSet rs = null;
 		try {
-			Connection connection = DriverManager.getConnection("jdbc:hsqldb:mem:wikipulsememdb", "SA", "");
-			ResultSet rs = null;
-			try {
-				PreparedStatement psq = connection.prepareStatement("SELECT * FROM mostreadnews WHERE article=?");
-				psq.clearParameters();
-				psq.setString(1,News);
-				rs = psq.executeQuery();
+				connection = getConnection();
+				prepStatement = connection.prepareStatement("SELECT * FROM mostreadnews WHERE article=?");
+				prepStatement.clearParameters();
+				prepStatement.setString(1,News);
+				rs = prepStatement.executeQuery();
 				int counter = 1;				
 				if (rs != null && rs.next()){
 					counter = rs.getInt(2) + 1;
-					psq.close();
-					psq = connection.prepareStatement("UPDATE mostreadnews SET numberofclicks=? WHERE article=? ");
-					psq.setString(1,Integer.toString(counter));
-					psq.setString(2,News);
+					prepStatement.close();
+					prepStatement = connection.prepareStatement("UPDATE mostreadnews SET numberofclicks=? WHERE article=? ");
+					prepStatement.setString(1,Integer.toString(counter));
+					prepStatement.setString(2,News);
 				}		
 				else {
-					psq.close();
-					psq = connection.prepareStatement("INSERT INTO mostreadnews VALUES(?,?)");
-					psq.clearParameters();
-					psq.setString(1,News);
-					psq.setString(2,Integer.toString(counter));					
+					prepStatement.close();
+					prepStatement = connection.prepareStatement("INSERT INTO mostreadnews VALUES(?,?)");
+					prepStatement.clearParameters();
+					prepStatement.setString(1,News);
+					prepStatement.setString(2,Integer.toString(counter));					
 				}
-				psq.executeUpdate();					
-			} catch (SQLException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}		
+				prepStatement.executeUpdate();					
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
+		}finally{
+			cleanup(connection, prepStatement);
 		}
 	}
 	
 	public static List<News_Counter> getMostReadNews(){
 		List<News_Counter> resultList = new ArrayList<News_Counter>();
+		PreparedStatement prepStatement = null;
+		ResultSet rs = null;
 		try {
-			Connection connection = DriverManager.getConnection("jdbc:hsqldb:mem:wikipulsememdb", "SA", "");
-			ResultSet rs = null;
-			PreparedStatement psq = connection.prepareStatement("SELECT TOP 10 * FROM mostreadnews ORDER BY numberofclicks DESC");
-			rs = psq.executeQuery();		
+			connection = getConnection();
+			prepStatement = connection.prepareStatement("SELECT TOP 10 * FROM mostreadnews ORDER BY numberofclicks DESC");
+			rs = prepStatement.executeQuery();		
 			while (rs.next()) {
 				News_Counter news = new News_Counter(rs.getString(1), rs.getInt(2));
 				resultList.add(news);
@@ -155,6 +201,8 @@ public class HsqldbManager {
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
+		}finally{
+			cleanup(connection, prepStatement);
 		}
 		return resultList;
 	}
