5425c52a3e2465dd88c6ea9c228d893372947fd3
Harrison Mfula <hmfula@yahoo.com>
2013-01-12 16:04:02 +0200

    Refactor: added different closing mechanisms based on available open resources

--- src/com/coin2012/wikipulse/extraction/hsqldb/HsqldbManager.java
+++ src/com/coin2012/wikipulse/extraction/hsqldb/HsqldbManager.java
@@ -17,17 +17,17 @@ import com.coin2012.wikipulse.extraction.utils.TimestampGenerator;
 import com.coin2012.wikipulse.extraction.utils.models.Change;
 
 public class HsqldbManager {
-	static Logger logger = Logger.getLogger(HsqldbManager.class.getSimpleName());
-	
-	
+	static Logger logger = Logger
+			.getLogger(HsqldbManager.class.getSimpleName());
+
 	public static String getTimestampForLastSavedChange() {
 		String timestamp = null;
-		Connection connection = null;
-		PreparedStatement prepStatement = null;
+		ResultSet rs = null;
 		try {
-			connection = getConnection();
-			prepStatement = connection.prepareStatement("SELECT * FROM changes");
-			ResultSet rs = prepStatement.executeQuery();
+			Connection connection = getConnection();
+			PreparedStatement prepStatement = connection
+					.prepareStatement("SELECT * FROM changes");
+			rs = prepStatement.executeQuery();
 			while (rs.next()) {
 				if (rs.isFirst()) {
 					timestamp = rs.getString(1);
@@ -36,100 +36,73 @@ public class HsqldbManager {
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		}finally{
-			cleanup(connection, prepStatement);	
+		} finally {
+			closeDatabaseConnectionsFromResultSet(rs);
 		}
 		return timestamp;
 	}
-	
+
 	public static void saveChangesToMemDB(List<Change> changes) {
 		Connection connection = null;
 		PreparedStatement prepStatement = null;
 		try {
-		 connection = getConnection();
-			 prepStatement = connection.prepareStatement("INSERT INTO changes VALUES (?,?)");
+			connection = getConnection();
+			prepStatement = connection
+					.prepareStatement("INSERT INTO changes VALUES (?,?)");
 			for (Change change : changes) {
 				try {
 					prepStatement.clearParameters();
-					prepStatement.setString(1, TimestampGenerator.generateTimestampFromWikipediaTimestamp(change.getTimestamp()));
+					prepStatement.setString(1, TimestampGenerator
+							.generateTimestampFromWikipediaTimestamp(change
+									.getTimestamp()));
 					prepStatement.setString(2, change.getTitle());
 					prepStatement.executeUpdate();
 				} catch (SQLException e) {
-					logger.info("Ignored double: " + change.getTitle() + " " + change.getTimestamp());
+					logger.info("Ignored double: " + change.getTitle() + " "
+							+ change.getTimestamp());
 				}
 			}
 		} catch (SQLException e) {
-			logger.info("Failed to create a Preparedstatement because of  " + e.getCause());
+			logger.info("Failed to create a Preparedstatement because of  "
+					+ e.getCause());
 			e.printStackTrace();
-		}finally{
-			cleanup(connection, prepStatement);	
+		} finally {
+			closeDatabaseConnections(connection, prepStatement);
 		}
 	}
 
-	private static void cleanup(Connection connection, Statement prepStatement) {
-		try {
-		if(prepStatement != null){
-				prepStatement.close();
-			} 
-		if(connection != null){
-			connection.close();
-		} 
-		}catch (SQLException e) {
-			logger.info("Failed to close the connection to the database because of : " + e.getCause() );
-			e.printStackTrace();
-		}
-	}
-
-	private static Connection getConnection() throws SQLException {
-		return  DriverManager.getConnection("jdbc:hsqldb:mem:wikipulsememdb", "SA", "");
-	}
+	
 
 	public static void clearOldChangesFromMemDB(String timestamp) {
 		logger.info("Removing all changes from DB older than " + timestamp);
 		long timeAsLong = Long.valueOf(timestamp);
-		
+
 		ResultSet rs = getAllChangesFromMemDB();
 		try {
 			while (rs.next()) {
-				if(rs.getLong(1) < timeAsLong){
+				if (rs.getLong(1) < timeAsLong) {
 					rs.deleteRow();
 				}
 			}
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		}finally{
-			try {
-				cleanup(rs.getStatement().getConnection(), rs.getStatement());
-			} catch (SQLException e) {
-				logger.info("Failed to close the connection to the database because of : " + e.getCause() );
-				e.printStackTrace();
-			}	
+		} finally {
+			closeDatabaseConnectionsFromResultSet(rs);
 		}
-		
+
 	}
 
-	private static ResultSet getAllChangesFromMemDB() {
-		ResultSet rs = null;
-		try {
-				
-				Connection connection = getConnection();
-				PreparedStatement	prepStatement = connection.prepareStatement("SELECT * FROM changes for update");
-				rs = prepStatement.executeQuery();
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-		return rs;
-	}
+	
 
 	public static HashMap<String, AggregatedChanges> getAllAggregatedChangesFromMemDB() {
 		HashMap<String, AggregatedChanges> map = new HashMap<String, AggregatedChanges>();
-		ResultSet rs= null;
-		
+		ResultSet rs = null;
 		try {
-//			connection = getConnection();
-//			prepStatement = connection.prepareStatement("SELECT * FROM changes for update");
-//			ResultSet rs = prepStatement.executeQuery();
+			// connection = getConnection();
+			// prepStatement =
+			// connection.prepareStatement("SELECT * FROM changes for update");
+			// ResultSet rs = prepStatement.executeQuery();
 			rs = getAllChangesFromMemDB();
 			while (rs.next()) {
 				String title = rs.getString(2);
@@ -143,72 +116,126 @@ public class HsqldbManager {
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		}finally{
-			try {
-				cleanup(rs.getStatement().getConnection(), rs.getStatement());
-			} catch (Exception e) {
-				logger.info("Failed to close the connection to the database because of : " + e.getCause() );
-				e.printStackTrace();
-			}	
+		} finally {
+			closeDatabaseConnectionsFromResultSet(rs);
 		}
 		return map;
 	}
-	
-	public static void saveUserInteractionInDB(String News){
-		Connection connection = null;
+
+	public static void saveUserInteractionInDB(String News) {
 		PreparedStatement prepStatement = null;
-		ResultSet rs = null;
+		Connection connection = null;
 		try {
-				connection = getConnection();
-				prepStatement = connection.prepareStatement("SELECT * FROM mostreadnews WHERE article=?");
+		    connection = getConnection();
+		    prepStatement  = connection
+					.prepareStatement("SELECT * FROM mostreadnews WHERE article=?");
+			prepStatement.clearParameters();
+			prepStatement.setString(1, News);
+			ResultSet	rs = prepStatement.executeQuery();
+			int counter = 1;
+			if (rs != null && rs.next()) {
+				counter = rs.getInt(2) + 1;
+				prepStatement.close();
+				prepStatement = connection
+						.prepareStatement("UPDATE mostreadnews SET numberofclicks=? WHERE article=? ");
+				prepStatement.setString(1, Integer.toString(counter));
+				prepStatement.setString(2, News);
+			} else {
+				prepStatement.close();
+				prepStatement = connection
+						.prepareStatement("INSERT INTO mostreadnews VALUES(?,?)");
 				prepStatement.clearParameters();
-				prepStatement.setString(1,News);
-				rs = prepStatement.executeQuery();
-				int counter = 1;				
-				if (rs != null && rs.next()){
-					counter = rs.getInt(2) + 1;
-					prepStatement.close();
-					prepStatement = connection.prepareStatement("UPDATE mostreadnews SET numberofclicks=? WHERE article=? ");
-					prepStatement.setString(1,Integer.toString(counter));
-					prepStatement.setString(2,News);
-				}		
-				else {
-					prepStatement.close();
-					prepStatement = connection.prepareStatement("INSERT INTO mostreadnews VALUES(?,?)");
-					prepStatement.clearParameters();
-					prepStatement.setString(1,News);
-					prepStatement.setString(2,Integer.toString(counter));					
-				}
-				prepStatement.executeUpdate();	
-				rs.close();
+				prepStatement.setString(1, News);
+				prepStatement.setString(2, Integer.toString(counter));
+			}
+			prepStatement.executeUpdate();
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		}finally{
-			cleanup(connection, prepStatement);
+		} finally {
+			closeDatabaseConnections(connection, prepStatement);
+		}
+	}
+
+
+
+	public static List<News_Counter> getMostReadNews() {
+		List<News_Counter> resultList = new ArrayList<News_Counter>();
+		ResultSet rs = null;
+		try {
+			Connection connection = getConnection();
+			PreparedStatement prepStatement = connection
+					.prepareStatement("SELECT TOP 10 * FROM mostreadnews ORDER BY numberofclicks DESC");
+			rs = prepStatement.executeQuery();
+			while (rs.next()) {
+				News_Counter news = new News_Counter(rs.getString(1),
+						rs.getInt(2));
+				resultList.add(news);
+			}
+
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			closeDatabaseConnectionsFromResultSet(rs);
+		}
+		return resultList;
+	}
+	
+	private static void closeDatabaseConnections(Connection connection, Statement prepStatement) {
+		try {
+			if (prepStatement.getResultSet() != null) {
+				prepStatement.getResultSet().close();
+			}
+			if (prepStatement != null) {
+				prepStatement.close();
+			}
+			if (connection != null) {
+				connection.close();
+			}
+		} catch (SQLException e) {
+			logger.info("Failed to close the connection to the database because of : "
+					+ e.getCause());
+			e.printStackTrace();
+		}
+	}
+
+	private static void closeDatabaseConnectionsFromResultSet(ResultSet rs) {
+		try {
+			if (rs != null) {
+				rs.close();
+			}
+
+			if (rs.getStatement() != null) {
+				rs.getStatement().close();
+			}
+			if (rs.getStatement().getConnection() != null) {
+				rs.getStatement().getConnection().close();
+			}
+
+		} catch (Exception e) {
+			logger.info("Failed to close the connection to the database because of : "
+					+ e.getCause());
+			e.printStackTrace();
 		}
 	}
 	
-	public static List<News_Counter> getMostReadNews(){
-		List<News_Counter> resultList = new ArrayList<News_Counter>();
-		Connection connection = null;
-		PreparedStatement prepStatement = null;
+	private static ResultSet getAllChangesFromMemDB() {
 		ResultSet rs = null;
 		try {
-			connection = getConnection();
-			prepStatement = connection.prepareStatement("SELECT TOP 10 * FROM mostreadnews ORDER BY numberofclicks DESC");
-			rs = prepStatement.executeQuery();		
-			while (rs.next()) {
-				News_Counter news = new News_Counter(rs.getString(1), rs.getInt(2));
-				resultList.add(news);
-			}
-			rs.close();
+
+			Connection connection = getConnection();
+			PreparedStatement prepStatement = connection
+					.prepareStatement("SELECT * FROM changes for update");
+			rs = prepStatement.executeQuery();
 		} catch (SQLException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
-		}finally{
-			cleanup(connection, prepStatement);
 		}
-		return resultList;
+		return rs;
+	}
+	
+	private static Connection getConnection() throws SQLException {
+		return DriverManager.getConnection("jdbc:hsqldb:mem:wikipulsememdb",
+				"SA", "");
 	}
 }
