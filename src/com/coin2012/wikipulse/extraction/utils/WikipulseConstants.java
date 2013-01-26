f3edcbccc33b23d539ad2b695050660611f7da5d
Harrison Mfula <hmfula@yahoo.com>
2013-01-26 23:24:30 +0200

    Implement:added a common class where to store all application constants

---
+++ src/com/coin2012/wikipulse/extraction/utils/WikipulseConstants.java
@@ -0,0 +1,12 @@
+package com.coin2012.wikipulse.extraction.utils;
+
+/**
+ * 
+ * This class store constants used throughout the application.
+ *
+ */
+public abstract class WikipulseConstants {
+	public final static String WIKIPEDIA_API_BASE_URL = "http://en.wikipedia.org/w/api.php";
+	public final static  int MAXIMUM_ALLOWED_NUMBER_OF_SENTENCES = 20;
+
+}
