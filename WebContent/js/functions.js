65f4d800e0bd76f21db2a4525d3db03c9be57b3a
tobilalala <sade1337@web.de>
2013-01-30 15:43:25 +0100

    ws stuff

--- WebContent/js/functions.js
+++ WebContent/js/functions.js
@@ -1,5 +1,5 @@
 /* get current domain and create URLs to REST - routes */
-var wp_service_url = window.location.toString().substring(0, window.location.toString().lastIndexOf('/'));
+var wp_service_url = window.location.toString().substring(0, window.location.toString().lastIndexOf("/"));
 var wp_service_route_news = wp_service_url + "/news";
 var wp_service_route_categories = wp_service_url + "/categories";
 var wp_service_route_changes = wp_service_url + "/changes";
@@ -42,26 +42,26 @@ $(document).ready(function() {
 });
 
 /* catch click event of BOTTOM navigation (AboutUs,ContactUs) */
-$('.bottom_nav_link').click(function(){
+$(".bottom_nav_link").click(function(){
 	/* retrieve link-information, remove active-class from TOP navigation, load .html - file */
 	var value = $(this).attr("href");
-	$('.nav>li.active').removeClass('active');
-	loadDoc(value.replace('#','') + '.html');
+	$(".nav>li.active").removeClass("active");
+	loadDoc(value.replace("#","") + ".html");
 });
 
 /* catch click event of TOP navigation (dynamic categories) */
-$('#navigation').on('click','.nav_link', function(){
+$("#navigation").on("click",".nav_link", function(){
 	/* retrieve link-information, remove active-class, load either home.html or dynamic content, set new active-class*/
 	var value = $(this).attr("href");
-	value = value.replace('#cat=','');
-	$(this).parent().siblings().removeClass('active');
-	if (value == "home" ) loadDoc('home.html');
-	else loadDoc('news.html');
-	$(this).parent().addClass('active');
+	value = value.replace("#cat=","");
+	$(this).parent().siblings().removeClass("active");
+	if (value == "home" ) loadDoc("home.html");
+	else loadDoc("news.html");
+	$(this).parent().addClass("active");
 });
 
 /* catch ENTER key (search form)  not used currently*/
-//$('.search-query').keyup(function (e) {
+//$(".search-query").keyup(function (e) {
 	/* prevent default which would cause a full page reload */
 //	e.preventDefault();
 //    if (e.keyCode == 13) {    	
@@ -69,35 +69,35 @@ $('#navigation').on('click','.nav_link', function(){
 //});
 
 /* catch search form event */
-//$('#search_form').submit(function (e) {
+//$("#search_form").submit(function (e) {
 	/* prevent page reload and load dynamic content based on search inpute value*/
 //	e.preventDefault();
-//    var value = $('#search_input').val();
-//    loadDoc('search.html?content=' + value);
+//    var value = $("#search_input").val();
+//    loadDoc("search.html?content=" + value);
 //});
 
 /* add click event to all news records in order, increase user interaction (counter) and load news detail*/
-$('#main_div').on("click",'.show_detail', function(){
-    var news_id = $(this).attr('newsid');
+$("#main_div").on("click",".show_detail", function(){
+    var news_id = $(this).attr("newsid");
     increaseViewCounter(news_id);
-    loadDoc('news_detail.html');
+    loadDoc("news_detail.html");
 });
 
-$('#most_read_stories').on("click",'.show_detail', function(){
-    var news_id = $(this).attr('newsid');
+$("#most_read_stories").on("click",".show_detail", function(){
+    var news_id = $(this).attr("newsid");
     increaseViewCounter(news_id);
-    loadDoc('news_detail.html');
+    loadDoc("news_detail.html");
 });
 
 /* set class=active for navigation item based on attribute href */
 function setActiveClass(href){
 	/* remove active from all items and then set it */
-	$('.top_nav li').each(function(){
-		$(this).removeClass('active');		
-	    if( ($(this).children('.nav_link').attr('href') !== undefined) &&
-    		($(this).children('.nav_link').attr('href').toLowerCase() == (("#cat=" + href).toLowerCase())) && 
+	$(".top_nav li").each(function(){
+		$(this).removeClass("active");		
+	    if( ($(this).children(".nav_link").attr("href") !== undefined) &&
+    		($(this).children(".nav_link").attr("href").toLowerCase() == (("#cat=" + href).toLowerCase())) && 
     		(href != "")){
-	    		$(this).addClass('active');
+	    		$(this).addClass("active");
 	    }
 	});
 }
@@ -105,24 +105,24 @@ function setActiveClass(href){
 /* load desired document into main_div */
 function loadDoc(source) {
     $.get(source, function(data) {
-    	$('#main_div').html(data);
+    	$("#main_div").html(data);
     });
 }
 
 /* load recent changes and display */
 function loadRecentChanges(minchanges){
 	$.ajax({
-	    type: 'GET',
+	    type: "GET",
 	    url: wp_service_route_changes + "?minchanges=" + minchanges.toString(),
-	    dataType: 'json',
+	    dataType: "json",
 	    success: function (data) {
-	    	$("#wait_recent_changes").html('');
-	    	$("#recent_changes ul").html('');
-	    	$("#recent_changes ul").append('<li class="nav-header">Recent Changes</li>');
+	    	$("#wait_recent_changes").html("");
+	    	$("#recent_changes ul").html("");
+	    	$("#recent_changes ul").append("<li class='nav-header'>Recent Changes</li>");
 	    	$.each(data,function(i,changes){
     			if(changes.title.indexOf(":") < 0)
     				{
-	    				var append_str = '<li><a href="http://' +  changes.url + '">' + changes.title + '&nbsp;(&nbsp;' + changes.count + '&nbsp;edits)&nbsp;' +'</a></li>';
+	    				var append_str = "<li><a href='http://" +  changes.url + "'>" + changes.title + "&nbsp;(&nbsp;" + changes.count + "&nbsp;edits)&nbsp;" +"</a></li>";
 		      			$("#recent_changes ul").append(append_str);	    				
     				}	    			
 	    	});
@@ -133,21 +133,30 @@ function loadRecentChanges(minchanges){
 /* get news detail from service and display */
 function loadNewsDetail(url_parameter){
 	var news_template = "<h3>news_title</h3>" + "<div id='news_id'>img_slide_show</div>" +
-	"<p>news_summary</p><a href='wikipedia_article'>see more on wikipedia</a>";
+	"<p>news_summary</p><a href='wikipedia_article'>see more on wikipedia</a><p class='p-small'>edited by:<br/> editor_list</p>";
     $.ajax({
-	    type: 'GET',
+	    type: "GET",
 	    url: wp_service_route_news + "/" + url_parameter,
-	    dataType: 'json',
+	    dataType: "json",
 	    success: function (news) {
-    		$("#wait").html('');
+    		$("#wait").html("");
 	    	if (news !== null){	 							
-				append_str = news_template;
-				document.title = news.pageTitle;
-				append_str = append_str.replace(/news_id/g, '"' + news.id + '"');
+				append_str = news_template;				
+				append_str = append_str.replace(/news_id/g, "'" + news.id + "'");
 				append_str = append_str.replace(/news_title/g,news.pageTitle);
 				append_str = append_str.replace(/img_slide_show/g,createImageSlideshow(news.id,0,news.imageUrlList));
 				append_str = append_str.replace(/news_summary/g,news.news);
-				append_str = append_str.replace(/wikipedia_article/g, news.BASE_URL + news.pageTitle);				
+				append_str = append_str.replace(/wikipedia_article/g, news.BASE_URL + news.pageTitle);
+				var editor_list = "";
+				if (news.editors !== undefined){ 
+					if (news.editors.length > 1) alert("editors: " + news.editors.length);
+					for ( var i = 0; i < news.editors.length; i++) {
+						editor_list += news.editors[i].user;
+						if ((i+1)<news.editors.length) editor_list += ", "; 
+					}			
+				}
+				append_str = append_str.replace(/editor_list/g,editor_list);
+				
 	    		$("#wp_service_news_detail_results").html(append_str);	  
 	    	}
 	    	else {
@@ -155,7 +164,7 @@ function loadNewsDetail(url_parameter){
 	    	}
 	    },
 	    error: function(){
-	    	$("#wait").html('');
+	    	$("#wait").html("");
 	    	$("#wp_service_news_detail_results").html("sorry, news not found");	
 	    }
 	});
@@ -177,18 +186,18 @@ function loadNewsImplementation(url){
 	var news_template_small = "<h6><a href='#newsid=news_id' class='show_detail' news_id='news_id'>news_title</a></h6>" +
 							"<div id='news_id'>img_slide_show</div>" +
 							"<p class='p-small'>news_shortsummary<a href='#newsid=news_id' class='show_detail' newsid='news_id'>&nbsp;more information</a></p>";
-	if (getUrlVars()["cat"] !== undefined) document.title = getUrlVars()["cat"];
+	
 	$.ajax({
-	    type: 'GET',
+	    type: "GET",
 	    url: url,
-	    dataType: 'json',
+	    dataType: "json",
 	    success: function (data) {
-	    	$("#wait").html('');
+	    	$("#wait").html("");
 	    	data.sort(function(a,b){ return parseInt(b.yesterdaysRelevance*100) - parseInt(a.yesterdaysRelevance*100);});
-	    	var append_str = '';
+	    	var append_str = "";
 	    	var counter = 0;
 	    	var subcounter = 0;
-	    	var news_rows = '';
+	    	var news_rows = "";
 	    	$.each(data,function(i,news){	    		
     			found = false;
     			append_str = (counter == 0 ) ? news_template_big : news_template_small;
@@ -213,51 +222,51 @@ function loadNewsImplementation(url){
 	    			found = true;
     			}
     			if(counter>2 && (found == false) && (subcounter==0)){
-    				news_rows += '<div class="row-fluid"><div class="span3">' + append_str +'</div>'; // only for first news item in row
+    				news_rows += "<div class='row-fluid'><div class='span3'>" + append_str +"</div>"; // only for first news item in row
     				subcounter += 1;
 	    			counter += 1;
 	    			found = true;
 				}
     			if(counter>2 && (found == false) && (subcounter==1)){
-    				news_rows += '<div class="span3">' + append_str +'</div>';
+    				news_rows += "<div class='span3'>" + append_str +"</div>";
     				subcounter += 1;
 	    			counter += 1;
 	    			found = true;
 				}	
     			if(counter>2 && (found == false) && (subcounter==2)){
-    				news_rows += '<div class="span3">' + append_str +'</div></div>';
+    				news_rows += "<div class='span3'>" + append_str + "</div></div>";
     				subcounter = 0; // only 3rd news item
 	    			counter += 1;
 	    			found = true;
 				}
 	    	}); // close for-each loop
 	    	if(subcounter != 0){
-	    		news_rows += '</div>';	    		
+	    		news_rows += "</div>";	    		
 	    	}
 	    	$("#row2").append(news_rows);
 	    },
 	    error: function(){
-	    	$("#wait").html('sorry, category not available');
-	    	//alert('sorry, category not available');
-	    }	    
+	    	$("#wait").html("sorry, category not available");
+	    	//alert("sorry, category not available");
+	    } 
 	});
 }
 
 /* search functionality on wikipulse - currently not used */
 //function search_wikipulse_service(){
-//	var search_input =$('#search_input').val();
+//	var search_input =$("#search_input").val();
 	//alert(wp_service_url_free_text_search + search_input);
 //	$.ajax({
-//	    type: 'GET',
+//	    type: "GET",
 //	    url: wp_service_url_free_text_search + search_input,
-//	    dataType: 'json',
+//	    dataType: "json",
 //	    success: function (data) {
 //	    	$.each(data,function(i,page_snippet){
-//   			var append_str = '<div class="row-fluid"><div class="span6 offset3">';
-//				append_str += '<div id="_content'+i+'">';
-//				append_str += '<a href="' +  page_snippet.urlToFullPage + '">' +  page_snippet.title + '</a>';
-//				append_str += '<p class="p-small">' + page_snippet.snippet + '</p>';
-//     			append_str += '</div></div></div><hr>';
+//   			var append_str = "<div class='row-fluid'><div class='span6 offset3'>";
+//				append_str += "<div id='_content"+i+"'>";
+//				append_str += "<a href='" +  page_snippet.urlToFullPage + "'>" +  page_snippet.title + "</a>";
+//				append_str += "<p class='p-small'>" + page_snippet.snippet + "</p>";
+//     			append_str += "</div></div></div><hr>";
 //      			$("#wp_service_search_results").append(append_str);
 //	    	});
 //	    }
@@ -267,13 +276,13 @@ function loadNewsImplementation(url){
 /* get most read news functionality on wikipulse and display them */
 function loadMostReadNews(){
 	$.ajax({
-	    type: 'GET',
+	    type: "GET",
 	    url: wp_service_route_news + "?sort=views&limit=" + mostreadstories_limit,
-	    dataType: 'json',
+	    dataType: "json",
 	    success: function (data) {	    	
-	    	var append_str = '<div class="nav-header p-small">Most Read Stories</div>'+
-	    					'<div class="row-fluid"><div class="span12" style="text-align:center;">'+
-	    						'<table class="table table-condensed" style="margin-bottom:0px"><tr>';
+	    	var append_str = "<div class='nav-header p-small'>Most Read Stories</div>"+
+	    					"<div class='row-fluid'><div class='span12' style='text-align:center;'>"+
+	    						"<table class='table table-condensed' style='margin-bottom:0px'><tr>";
 	    	$.each(data,function(i,news){
     			if(news.pageTitle.indexOf(":") < 0)
     				{
@@ -289,61 +298,61 @@ function loadMostReadNews(){
 /* get category structure and load it if desired, e.g. bookmarks */
 function loadCategories(){
 	$.ajax({
-	    type: 'GET',
+	    type: "GET",
 	    url: wp_service_route_categories + "?limit=" + category_no_limit,
-	    dataType: 'json',
+	    dataType: "json",
 	    success: function (data) {	    	
 	    	$.each(data,function(i,category){
-	    		$('.top_nav').append($('<li/>').append($('<a/>', {
-	    		    'href': '#cat=' + category.title,
-	    		    'class': 'nav_link',
-	    		    'text': category.title
+	    		$(".top_nav").append($("<li/>").append($("<a/>", {
+	    		    "href": "#cat=" + category.id,
+	    		    "class": "nav_link",
+	    		    "text": category.title
 	    		})));
-	    		$('.top_nav').append($('<li/>', { 'class': "divider-vertical" }));
+	    		$(".top_nav").append($("<li/>", { "class": "divider-vertical" }));
 	    	});
 	    	if (window.location.toString().indexOf("#cat=", 0) > 0){
 	    		var current_doc = "";
-	    		current_doc = window.location.toString().substring(window.location.toString().lastIndexOf('#cat=')+5,window.location.toString().length).toLowerCase();
+	    		current_doc = window.location.toString().substring(window.location.toString().lastIndexOf("#cat=")+5,window.location.toString().length).toLowerCase();
 	    		//alert(current_doc);
 	    		switch(current_doc)
 	    		{
 	    			case "":
-	    				loadDoc('home.html');
+	    				loadDoc("home.html");
 	    				setActiveClass("home");
 	    				break;
-	    			case "Home":
-	    				loadDoc('home.html');
+	    			case "home":
+	    				loadDoc("home.html");
 	    				setActiveClass("home");
 	    				break;
 	    			case "aboutus":
-	    				loadDoc('aboutus.html');
+	    				loadDoc("aboutus.html");
 	    				setActiveClass("");
 	    				break;
 	    			case "contactus":
-	    				loadDoc('contactus.html');
+	    				loadDoc("contactus.html");
 	    				setActiveClass("");
 	    				break;
 	    			default:
-	    				loadDoc('news.html');
+	    				loadDoc("news.html");
 	    				setActiveClass(current_doc);
 	    				break;
 	    		}
 	    	}
 	    	else if (window.location.toString().indexOf("#newsid=", 0) > 0){
 	    		//var news_id = "";
-	    		//news_id = window.location.toString().substring(window.location.toString().lastIndexOf('#newsid=')+9,window.location.toString().length).toLowerCase();
+	    		//news_id = window.location.toString().substring(window.location.toString().lastIndexOf("#newsid=")+9,window.location.toString().length).toLowerCase();
 	    		//alert(news_id);
-	    		loadDoc('news_detail.html');
+	    		loadDoc("news_detail.html");
 	    	}
 	    	else {
-	    		//alert('load home, no parameter passed');
-	    		loadDoc('home.html');
+	    		//alert("load home, no parameter passed");
+	    		loadDoc("home.html");
 	    		setActiveClass("home");
 	    	} 
 	    },
 	    error: function(){
-	    	//alert('error, no result');
-			loadDoc('home.html');
+	    	//alert("error, no result");
+			loadDoc("home.html");
 			setActiveClass("home");
 	    }
 	});
@@ -352,62 +361,53 @@ function loadCategories(){
 /* increase view counter for news */
 function increaseViewCounter(newsID){	
 	$.ajax({
-	    type: 'PUT',
+	    type: "PUT",
 	    url: wp_service_route_news + "/" + newsID,
-	    dataType: 'json',
+	    dataType: "json",
 	    success: function (data) {
 	    }
-	});	
-	
-	/*var result = confirm("Do you want to open the Wikipedia-page: " + news);
-	if (result==true) {
-		//Logic to delete the item
-		window.open(wiki_url + news, '_blank');
-		window.focus();	
-	}*/
+	});
 }
 
-/* create HTML-code for image slide show in news category and news detail */
-function createImageSlideshow(newsId,news_counter,urlList){
-	var img_url_list_str = "";
-	//img_url_list_str = urlList;
-	if (jQuery.isEmptyObject(urlList) || urlList == "") img_url_list_str = wp_service_url + "/img/img_not_found.jpg";
+/* create HTML-code for image slide for news */
+function createImageSlideshow(newsId,news_counter,IMGurlList){
+	if ((jQuery.isEmptyObject(IMGurlList)) || (IMGurlList == "") || (IMGurlList == undefined) ){
+		IMGurlList[0] = wp_service_url + "/img/img_not_found.jpg";
+	}	
+	var div_slideshow = "<div id='myCarousel_" + newsId
+	+ "' class='carousel slide'><div class='carousel-inner'>";
 	
-	var div_slideshow = '<div id="myCarousel_' + newsId
-	+ '" class="carousel slide"><div class="carousel-inner">';
-	
-	var img_list = img_url_list_str.toString().split(',');
-	for(var j=0;j<img_list.length;j++){
+	for(var j=0;j<IMGurlList.length;j++){
 		if(j==0){
-			div_slideshow += '<div class="item active">';
+			div_slideshow += "<div class='item active'>";
 		}
 		else {
-			div_slideshow += '<div class="item">';
+			div_slideshow += "<div class='item'>";
 		} 
 		if (news_counter >= 3)
-			div_slideshow += '<img class="img-rounded img_size_news_small" src="'+ img_list[j] +'" alt="">';
+			div_slideshow += "<img class='img-rounded img_size_news_small' src='"+ IMGurlList[j] +"' alt=''>";
 		else if (news_counter >= 1)
-			div_slideshow += '<img class="img-rounded img_size_news_small" src="'+ img_list[j] +'" alt="">';
+			div_slideshow += "<img class='img-rounded img_size_news_small' src='"+ IMGurlList[j] +"' alt=''>";
 		else 
-			div_slideshow += '<img class="img-rounded img_size_news_big" src="'+ img_list[j] +'" alt="">';
+			div_slideshow += "<img class='img-rounded img_size_news_big' src='"+ IMGurlList[j] +"' alt=''>";
 		
-		div_slideshow += '</div>';
+		div_slideshow += "</div>";
     };
-    div_slideshow += '</div>';
-    div_slideshow += '<a class="left carousel-control" href="#myCarousel_'+ newsId + '" data-slide="prev">&lsaquo;</a>';
-    div_slideshow += '<a class="right carousel-control" href="#myCarousel_'+ newsId + '" data-slide="next">&rsaquo;</a>';
-    div_slideshow += '</div>';
+    div_slideshow += "</div>";
+    div_slideshow += "<a class='left carousel-control' href='#myCarousel_"+ newsId + "' data-slide='prev'>&lsaquo;</a>";
+    div_slideshow += "<a class='right carousel-control' href='#myCarousel_" + newsId + "' data-slide='next'>&rsaquo;</a>";
+    div_slideshow += "</div>";
     return div_slideshow;
 }
 
-/* returns url parameter as an array, whole parameter string must start with a '#' */
+/* returns url parameter as an array, whole parameter string must start with a "#" */
 function getUrlVars()
 {
     var vars = [], hash;
-    var hashes = window.location.href.slice(window.location.href.indexOf('#') + 1).split('&');
+    var hashes = window.location.href.slice(window.location.href.indexOf("#") + 1).split("&");
     for(var i = 0; i < hashes.length; i++)
     {
-        hash = hashes[i].split('=');
+        hash = hashes[i].split("=");
         vars.push(hash[0]);
         vars[hash[0]] = hash[1];
     }
@@ -445,8 +445,8 @@ function GetDateTime() {
 
     this.date = arguments.length == 0 ? new Date() : new Date(arguments);
 
-    this.weekdays = new Array('Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday');
-    this.months = new Array('January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December');
+    this.weekdays = new Array("Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday");
+    this.months = new Array("January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December");
     this.daySuf = new Array( "st", "nd", "rd", "th" );
 
     this.day = {
